199707137c57ba5c9912fc6682c85d6a3f0be841
navjotn406 <113278709+navjotn406@users.noreply.github.com>
2022-09-29 17:10:51 -0400

    first conversion

--- convert.kt
+++ convert.kt
@@ -1,6 +1,21 @@
 //Name : Navjot Singh
 //Student ID: A00264732
 
-fun main(){
-  println("Hello World")
+fun main()
+{
+  var num = readln().toDouble()
+
+  println("Enter a number: ")
+  /* var result:Float = 0.0 */
+  println("Enter the Unit")
+  var unit = readln()
+  if(unit == "km")
+  {
+  num = num * 0.62
+  println("The result is:" + num + "mi")
+
+  } else if (unit == "mi"){
+    num = num * 1.61
+  }
+
 }
