3e58df9c645e8bbfc74ac0749a3c100b4e9b0c66
navjotn406 <113278709+navjotn406@users.noreply.github.com>
2022-09-30 10:32:07 -0400

    for submission

--- convert.kt
+++ convert.kt
@@ -1,55 +1,82 @@
 //Name : Navjot Singh
 //Student ID: A00264732
+//jav1001-lab1
 
 fun main()
 {
-  //var num = readln().toDouble()
-
   println("Enter a number: ")
   var num = readln().toDouble()//variable declaraiton
-  //var value = readln()!!
-  /* var result:Float = 0.0 */
-  println("Enter the Unit")
+  println("Enter the Unit: ")
   var unit = readln() // imput unit
   if(unit == "km")
   {
   num = num * 0.62
   println("The result is:" + num +  "mi")//conversion
   }
-    else if (unit == "mi")
-    {
-    num = num * 1.61
-    println("The result is:" + num +  "km")
+
+  else if (unit == "mi")
+  {
+  num = num * 1.61
+  println("The result is:" + num +  "km")
   }
+
    else if (unit == "cm")
- {
+  {
    num = num * 0.39
    println("The result is:" + num  + "in")
- }
+  }
+
    else if (unit == "in")
-   {
+  {
    num = num * 2.54
    println("The result is:" + num + "cm")
- }
-else if (unit == "kg")
-{
- num = num * 2.2
- println("The result is :" +num  + "lb")
- }
- else if (unit == "lb")
- {
- num = num * 0.45
- println("The result is :" + num   +"kg")
-}
+  }
 
-else if (unit == "g")
-{
+   else if (unit == "kg")
+  {
+   num = num * 2.2
+   println("The result is :" +num  + "lb")
+  }
+
+   else if (unit == "lb")
+  {
+   num = num * 0.45
+   println("The result is :" + num   +"kg")
+  }
+
+  else if (unit == "g")
+  {
   num = num * 0.04
-println("The result is :"  + num  + "oz")
-}
- else if (unit == "oz")
- {
+  println("The result is :"  + num  + "oz")
+  }
+
+  else if (unit == "oz")
+  {
    num = num * 28.35
-println("The result is :" + num + "g")
+   println("The result is :" + num + "g")
+  }
+
+  else if (unit == "min")
+  {
+   num = num *60
+   println("The result is :"  + num  + "sec")
+  }
+
+  else if (unit == "sec")
+  {
+   num = num /60
+   println("The result is :" +num  + "min")
+  }
+
+  else if (unit == "byte")
+  {
+  num = num / 1000
+  println("The result is :"  +num  + "kilobyte")
+  }
+
+  else if (unit == "kilobyte")
+  {
+  num = num * 1000
+  println("The result is :" + num  + "byte")
+  }
  }
-}
